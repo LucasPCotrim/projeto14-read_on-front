be4317bcf94f1f87b36428af1bbd859b0435077a
kcandeloni <kcandeloni@gmail.com>
2022-09-16 19:34:08 -0300

    rota setProducts + carrinho

--- src/components/ProductsPage/ProductsBox.js
+++ src/components/ProductsPage/ProductsBox.js
@@ -8,23 +8,27 @@ export default function ProductsBox ({
     price,
     amount,
     }) {
-        console.log(title)
     return(
         <BookBox>
             <ImageBook src={image} alt={title}/>
             <p>{title}</p>
             <Price>
                 <p>R$ {price}</p>
+                <ButtonCart>
+                    <ion-icon name="cart-outline"></ion-icon>
+                </ButtonCart>
             </Price>
             
         </BookBox>);
 }
 
 const BookBox = styled.div`
+    position: relative;
     width: 180px;
     height: 240px;
     border-radius: 5px;
-
+    background-color: var(--primary-color);
+    box-shadow: 3px 3px 7px 1px rgba(17, 45, 78, 0.5);
     margin: 4px 6px;
     padding: 4px 4px;
 
@@ -50,4 +54,17 @@ const ImageBook = styled.img`
     width: 140px;
     height: 160px;
     border-radius: 2px;
+`;
+
+const ButtonCart = styled.div`
+    position: absolute;
+    ion-icons{
+        color: var(--primary-color);
+        right: 8px;
+        bottom: 8px;
+    }
+    color: var(--primary-color);
+    z-index: 1;
+    width: 20px;
+    height: 20px;
 `;
