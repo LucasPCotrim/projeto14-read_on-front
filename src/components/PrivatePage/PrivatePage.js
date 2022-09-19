6aa9b014f5f7bff28e066df1d5eafd1a07b04495
LucasPCotrim <lucaspcotrim@gmail.com>
2022-09-19 03:27:47 -0300

    fix: Corrected merge errors

--- src/components/PrivatePage/PrivatePage.js
+++ src/components/PrivatePage/PrivatePage.js
@@ -1,18 +1,10 @@
 import TopMenu from '../TopMenu/TopMenu';
 
-<<<<<<< HEAD
 export default function PrivatePage({children}) {
   return (
     <>
       <TopMenu />
       {children}
-=======
-export default function PrivatePage(props) {
-  return (
-    <>
-      <TopMenu />
-      {props.children}
->>>>>>> main
     </>
   );
 }
--- src/components/TopMenu/TopMenu.js
+++ src/components/TopMenu/TopMenu.js
@@ -31,15 +31,11 @@ function CartMenu({ setCartPopUpMenu }) {
           <button onClick={() => navigate('/cart')}>
             <h2>Visualizar Carrinho</h2>
           </button>
-<<<<<<< HEAD
-          <button onClick={() => navigate('/checkout')}>
-=======
           <button
             onClick={() => {
               setCartPopUpMenu(false);
               navigate('/checkout');
             }}>
->>>>>>> main
             <h2>Finalizar Compra</h2>
           </button>
         </div>
